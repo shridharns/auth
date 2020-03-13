fdc3a04d1f648a6d0aef73a35917316b108182e1
shridharns <laks@Shridhars-MacBook-Air.local>
2020-03-12 22:32:30 -0700

    Updating swagger doc

--- src/main/java/com/shridhar/auth/controllers/UserController.java
+++ src/main/java/com/shridhar/auth/controllers/UserController.java
@@ -16,35 +16,64 @@ import com.shridhar.auth.model.User;
 import com.shridhar.auth.model.UserWithPassword;
 import com.shridhar.auth.service.UserService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 public class UserController {
 
-    UserService userService;
+    private UserService userService;
 
     public UserController(@Autowired UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("/users")
+    @ApiOperation(value = "Return list of users", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved user list"),
+            @ApiResponse(code = 500, message = "Internal error while processing the request")
+    })
     private List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @ApiOperation(value = "Return the user by Id", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved user object"),
+            @ApiResponse(code = 500, message = "Internal error while processing the request")
+    })
     @GetMapping("/users/{id}")
     private User getUser(@PathVariable("id") int id) {
         return userService.getPersonById(id);
     }
 
+    @ApiOperation(value = "Delete the user by Id", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted the user"),
+            @ApiResponse(code = 500, message = "Internal error while processing the request")
+    })
     @DeleteMapping("/users/{id}")
     private void deleteUser(@PathVariable("id") int id) {
         userService.delete(id);
     }
 
+    @ApiOperation(value = "Update the user by Id", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved user list"),
+            @ApiResponse(code = 500, message = "Internal error while processing the request")
+    })
     @PostMapping("/users/{id}")
     private User updateUser(@RequestBody UserWithPassword user) {
         return userService.saveOrUpdate(user);
     }
 
+    @ApiOperation(value = "User sign up operation", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully created user"),
+            @ApiResponse(code = 500, message = "Internal error while processing the request")
+    })
     @PutMapping("/users")
     private User createUser(@RequestBody UserWithPassword user) {
         return userService.saveOrUpdate(user);
